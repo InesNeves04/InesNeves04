110ff392fae0140d1356cd95124b13c921f1af65
InesNeves04 <129083901+InesNeves04@users.noreply.github.com>
2024-02-06 15:28:43 +0000

    Update File1.c

--- File1.c
+++ File1.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main(void){
+  float l, w, h, v;
+  printf("L=?"); scanf("%f", &l);
+  printf("W=?"); scanf("%f", &w);
+  printf("H=?"); scanf("%f", &h);
+
+  v = l*w*h;
+  printf("Volume= %f", v);
+}
